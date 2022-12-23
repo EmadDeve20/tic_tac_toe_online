4a81f55f8aaf5704875e335ba2ecccb32c1c1c0b
EmadDeve20 <emaddeve20@gmail.com>
2022-12-24 00:34:14 +0330

    create the "client_setup" function
    setup client to connect to the server so I need a function.
    Is this name good? or not?
    ok we need time to think about it but it is ok right now and I use this

--- client/main.c
+++ client/main.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
-int port = 8013;
+int port = 8013, sock = 0, client_fd;
 char server_address[1024];
 char username[1024];
+struct sockaddr_in socket_address;
 
 void initial_settings();
+void client_setup();
+
 
 int main()
 {
 
     initial_settings();
+    client_setup();
 
 }
 
@@ -26,4 +33,34 @@ void initial_settings()
     printf("Enter your name: ");
     scanf("%1023s", username);
 
+}
+
+/*
+    setup client means connecting to the server
+    and 
+    start game!
+*/
+void client_setup()
+{
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("Socket Connection Error!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    socket_address.sin_family = AF_INET;
+    socket_address.sin_port = htons(port);
+
+    // Convert IPv4 from text to binary
+    if (inet_pton(AF_INET, server_address, &socket_address.sin_addr) <= 0)
+    {
+        perror("Invalid address or Address not supported\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((client_fd = connect(sock, (struct sockaddr*)&socket_address, sizeof(socket_address))) < 0)
+    {
+        perror("Connection Faild!\n");
+        exit(EXIT_FAILURE);
+    }
 }
