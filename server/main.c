bcd4f2e6b2faefde9f272c7be617db7ae89c246e
EmadDeve20 <emaddeve20@gmail.com>
2022-12-29 13:55:20 +0330

    kill bugs in the new functions

--- server/main.c
+++ server/main.c
@@ -32,7 +32,7 @@ void print_welcome_message();
 void setup_server();
 void requests_parser();
 void insert_user(char *username);
-int user_list_is_empty(usersPtr *users_list);
+int user_list_is_empty(const usersPtr users_list);
 int new_username_valid(char *);
 
 int main()
@@ -159,7 +159,7 @@ void insert_user(char *username)
 }
 
 // list_of_users is Empty??
-int user_list_is_empty(usersPtr *users_list)
+int user_list_is_empty(const usersPtr users_list)
 {
     return users_list == NULL;
 }
@@ -167,13 +167,13 @@ int user_list_is_empty(usersPtr *users_list)
 // the new username is valid??
 int new_username_valid(char *new_username)
 {
-    usersPtr *__users = &list_of_users;
+    usersPtr __users = list_of_users;
 
     while (!user_list_is_empty(__users))
     {
-        if (strcmp((*__users)->username, new_username) == 0)
+        if (strcmp((__users)->username, new_username) == 0)
             return 0; // This username Exists! So new username is not Valid! it is must be unique
-        __users = &(*__users)->nextUser;
+        __users = (__users)->nextUser;
     }
 
     return 1;
